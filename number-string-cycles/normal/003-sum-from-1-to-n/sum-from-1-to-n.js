22d6eb2b15f9fa69f4260c764a3185fad976d1ca
SelinJax <seljax92@gmail.com>
2025-09-26 16:12:08 +0300

    -add-tasks

---
+++ number-string-cycles/normal/003-sum-from-1-to-n/sum-from-1-to-n.js
@@ -0,0 +1,13 @@
+// Сума чисел від 1 до N (sum-from-1-to-n)
+// Напиши функцію sumToN(n), яка повертає суму всіх чисел від 1 до n включно.
+// (Використай цикл for або while).
+
+function sumToN(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+console.log(sumToN(5));
