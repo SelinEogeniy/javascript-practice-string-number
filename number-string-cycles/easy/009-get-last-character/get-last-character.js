fbbcbaaf40c755b6004ecf29f5dbbeb31abe3af4
SelinJax <seljax92@gmail.com>
2025-09-27 23:00:25 +0300

    -add-task

---
+++ number-string-cycles/easy/009-get-last-character/get-last-character.js
@@ -0,0 +1,10 @@
+// Останній символ рядка (get-last-character)
+// Напиши функцію getLastChar(str), яка повертає останній символ рядка.
+
+function getLastChar(str) {
+  return str[str.length - 1];
+}
+// Приклади:
+
+console.log(getLastChar("JavaScript")); // "t"
+console.log(getLastChar("hello")); // "o"
