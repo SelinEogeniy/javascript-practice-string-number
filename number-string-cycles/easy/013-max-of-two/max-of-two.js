1f3ef8f0529e5c5dee226a01a94d364c482169c0
SelinJax <seljax92@gmail.com>
2025-09-29 18:20:42 +0300

    -add-task

---
+++ number-string-cycles/easy/013-max-of-two/max-of-two.js
@@ -0,0 +1,9 @@
+// Максимум з двох чисел (max-of-two)
+// Напиши функцію maxOfTwo(a, b), яка повертає більше з двох чисел.
+
+function maxOfTwo(a, b) {
+  return a > b ? a : b;
+}
+
+console.log(maxOfTwo(3, 7)); // 7
+console.log(maxOfTwo(10, 5)); // 10
