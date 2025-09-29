4eb568460784af7d0bdefdf681b73b3c3ab19165
SelinJax <seljax92@gmail.com>
2025-09-29 18:27:33 +0300

    -add-task

---
+++ number-string-cycles/easy/014-is-even/is-even.js
@@ -0,0 +1,9 @@
+// Перевірка на парність (is-even)
+// Напиши функцію isEven(num), яка повертає true, якщо число парне, і false — якщо непарне.
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(4)); // true
+console.log(isEven(7)); // false
