9cb6b4b3e67099db3fc4747ae73f7ce560f05aca
SelinJax <seljax92@gmail.com>
2025-09-30 22:29:02 +0300

    -add-task

---
+++ number-string-cycles/easy/018-square-number/square-number.js
@@ -0,0 +1,8 @@
+// todo Напиши функцію squareNumber(num), яка повертає квадрат числа.
+
+function squareNumber(num) {
+  return Math.pow(num, 2);
+}
+
+console.log(squareNumber(5)); // 25
+console.log(squareNumber(9)); // 81
