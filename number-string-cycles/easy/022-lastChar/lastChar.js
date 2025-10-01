03d6c5dda6aa388fbcc8c76d924ea2ed04109100
SelinJax <seljax92@gmail.com>
2025-10-01 22:56:50 +0300

    -new-task

---
+++ number-string-cycles/easy/022-lastChar/lastChar.js
@@ -0,0 +1,10 @@
+// 2. Останній символ
+
+// Напиши функцію lastChar(str), яка повертає останній символ переданого рядка.
+
+function lastChar(str) {
+  return str[str.length - 1];
+}
+
+console.log(lastChar("Hello")); // "o"
+console.log(lastChar("JavaScript")); // "t"
