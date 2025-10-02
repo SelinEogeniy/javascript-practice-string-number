b58e9aa87e3677a7093585385e8c2bd7422063d4
SelinJax <seljax92@gmail.com>
2025-10-02 20:57:55 +0300

    -add-task

---
+++ number-string-cycles/easy/024-is-adult/is-adult.js
@@ -0,0 +1,9 @@
+// Check Adult (is-adult)
+// Напиши функцію isAdult(age), яка повертає true, якщо age >= 18, інакше false.
+
+function isAdult(age) {
+  return age >= 18;
+}
+
+console.log(isAdult(20)); // true
+console.log(isAdult(16)); // false
