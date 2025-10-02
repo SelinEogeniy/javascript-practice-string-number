a339576e38ff0c90a8a538d948e212963a95e906
SelinJax <seljax92@gmail.com>
2025-10-02 21:03:48 +0300

    -add-task

---
+++ number-string-cycles/easy/026-count-down/count-down.js
@@ -0,0 +1,9 @@
+// Count Down (count-down)
+// Напиши функцію countDown(n), яка виводить числа від n до 1 у консоль.
+
+function countDown(n) {
+  for (let i = n; i >= 1; i--) {
+    console.log(i);
+  }
+}
+countDown(5);
