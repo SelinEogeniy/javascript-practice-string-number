fea1dd90186a53282f9ca9c9d1927bbc2ea3042e
SelinJax <seljax92@gmail.com>
2025-10-09 15:42:22 +0300

    -addtask

---
+++ number-string-cycles/normal/018-sum-squares-odd/sum-squares-odd.js
@@ -0,0 +1,8 @@
+// Sum of Squares of Odds (sum-squares-odd)
+
+// Напиши функцію sumSquaresOdd(n), яка обчислює суму квадратів усіх непарних чисел від 1 до n.
+
+// Приклад:
+
+console.log(sumSquaresOdd(5)); // 35 (1² + 3² + 5²)
+console.log(sumSquaresOdd(10)); // 165 (1²+3²+5²+7²+9²)
