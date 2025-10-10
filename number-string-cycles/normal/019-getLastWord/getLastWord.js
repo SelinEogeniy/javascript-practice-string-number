d6e524c52bce01b2f6a36f101dc0418c5c579286
SelinJax <seljax92@gmail.com>
2025-10-10 23:05:13 +0300

    -new-task

--- number-string-cycles/normal/019-getLastWord/getLastWord.js
+++ number-string-cycles/normal/019-getLastWord/getLastWord.js
@@ -6,5 +6,10 @@
 function getLastWord(str) {
   return str.slice(str.lastIndexOf(" ") + 1);
 }
+//~ var 2
+function getLastWord(str) {
+  const words = str.split(" ");
+  return words[words.length - 1];
+}
 
 console.log(getLastWord("I love JavaScript")); // "JavaScript"
