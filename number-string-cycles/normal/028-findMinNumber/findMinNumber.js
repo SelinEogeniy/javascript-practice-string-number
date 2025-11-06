4266880e8a918eeddf670fee650c95f3149391af
SelinJax <seljax92@gmail.com>
2025-11-06 02:31:19 +0200

    -newTask

---
+++ number-string-cycles/normal/028-findMinNumber/findMinNumber.js
@@ -0,0 +1,9 @@
+// 2. findMinNumber.js 🟢 easy
+// ✳️ Напиши функцію, яка приймає три числа і повертає найменше з них.
+// ➡️ Приклад: findMinNumber(4, 9, 1) → 1
+
+function findMinNumber(a, b, c) {
+  return Math.min(a, b, c);
+}
+
+console.log(findMinNumber(4, -2, 1));
