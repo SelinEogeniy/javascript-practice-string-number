4e394aba2ff53b081287be8c38542eb0ce659d6e
SelinJax <seljax92@gmail.com>
2025-12-10 08:46:03 +0200

    -newTask

---
+++ number-string-cycles/normal/034-removeDuplicates/removeDuplicates.js
@@ -0,0 +1,22 @@
+// 4) removeDuplicates.js — (normal)
+
+// Приймає масив чисел.
+// Повертає новий масив без дублікатів, але без використання Set.
+
+// Приклад:
+// removeDuplicates([1,2,2,3,1]) → [1,2,3]
+
+//~Variant 1
+// function removeDuplicates(arr) {
+//   let newArr = [];
+//   for (let item of arr) {
+//     if (!newArr.includes(item)) {
+//       newArr.push(item);
+//     }
+//   }
+//   return newArr;
+// }
+
+//~Variant 2 (filter)
+
+console.log(removeDuplicates([1, 2, 2, 3, 1]));
